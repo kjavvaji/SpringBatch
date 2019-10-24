46ccd14d853beec9e55db20017a54fec127e7ec8
Koteswara Rao Javvaji <kjavvaji@corp.miraclesoft.com>
2019-10-24 15:30:32 -0400

    Adding Batch Workspace

---
+++ src/main/java/com/learn/io/SpringBatchOneApplication.java
@@ -0,0 +1,13 @@
+package com.learn.io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/io/SpringBatchOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.io;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
